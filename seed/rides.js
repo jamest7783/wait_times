5c7b43606ba665e8fecb9e42784f399b3ef97019
James Jewitt <jamestjewitt@gmail.com>
2022-07-21 10:26:05 -0400

    Establish db

--- seed/rides.js
+++ seed/rides.js
@@ -5,6 +5,37 @@ const Ride = require('../models/ride')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
+
+    const att0 = await new Brand({
+        name: 'Apple',
+        url: 'https://www.apple.com'
+      })
+      brand1.save()
+    
+      const brand2 = await new Brand({
+        name: 'Vespa',
+        url: 'https://www.vespa.com'
+      })
+      brand2.save()
+    
+      const brand3 = await new Brand({
+        name: 'New Balance',
+        url: 'https://www.newbalance.com'
+      })
+      brand3.save()
+    
+      const brand4 = await new Brand({
+        name: 'Tribe',
+        url: 'https://www.tribebicycles.com'
+      })
+      brand4.save()
+    
+      const brand5 = await new Brand({
+        name: 'Stumptown',
+        url: 'https://www.stumptowncoffee.com'
+      })
+      brand5.save()
+
     const rides = [
         { name: 'a', description: 'a', image: 'a', attraction: att0._id },
         { name: 'a', description: 'a', image: 'a', attraction: att1._id },
